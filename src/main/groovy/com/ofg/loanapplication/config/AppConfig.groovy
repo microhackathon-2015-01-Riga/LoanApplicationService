1045b4d6ffe3790b29e7bda7385dc753c3e3ca93
Vadims Jagodins <vadims.jagodins@4finance.com>
2015-01-13 16:34:01 +0200

    Removed obsolete EnableMicroservice annotation

--- src/main/groovy/com/ofg/loanapplication/config/AppConfig.groovy
+++
@@ -1,9 +0,0 @@
-package com.ofg.loanapplication.config
-
-import com.ofg.infrastructure.config.EnableMicroservice
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@EnableMicroservice
-class AppConfig {
-}
